0a209b68a39ef09a83912977038c32760abc7df9
Aarav Sambyal <iamaaravsambyal@gmail.com>
2025-12-10 21:37:23 +0530

    added form validation

--- docs/contact/contact.php
+++ docs/contact/contact.php
@@ -43,7 +43,7 @@
                 <input type="email" name="email" id="email" placeholder="your email..."><br><br>
 
                 <label for="phone"><b>Phone no</b></label><br>
-                <input type="number" name="phone" id="phone" placeholder="your phone no..."><br><br>
+                <input type="text" name="phone" id="phone" placeholder="your phone no..."><br><br>
 
                 <label for="address"><b>Address</b></label><br>
                 <input type="text" name="address" id="address" placeholder="your address..."><br><br>
@@ -51,6 +51,37 @@
                 <button type="submit"><b>Send Message</b></button>
             </form>
         </div>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $address = $_POST['address'];
+            $hasError = false;
+
+            if (empty($name)) {
+                echo "<p class='error'>Username is required</p>";
+                $hasError = true;
+            }
+
+            if (strlen($phone) != 10) {
+                echo "<p class='error'> Please provide valid number</p>";
+                $hasError = true;
+            }
+
+            if (empty($email)) {
+                echo "<p class='error'>Email is required</p>";
+                $hasError = true;
+            }
+
+            if (!$hasError) {
+                echo "<p class='value'> $name </p>";
+                echo "<p class='value'>$email </p>";
+                echo "<p class='value'>$phone </p>";
+                echo "<p class='value'>$address </p>";
+            }
+        }
+        ?>
 
         <div id="info">
             <h3>Restaurant Location 📍</h3>
@@ -60,21 +91,11 @@
         </div>
     </section>
 
+
     <footer>
         <p>© 2025 SpiceHub</p>
     </footer>
-    <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $address = $_POST['address'];
 
-    echo "Registration Successful";
-    echo "$name <br>";
-    echo "$email <br>";
-    echo "$phone <br>";
-    echo "$address <br>";
-    ?>
 </body>
 
 </html>
